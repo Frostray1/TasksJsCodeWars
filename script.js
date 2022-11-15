9ba93df384526a17c991836bb086e8a8dca09a5a
Artem <a.bychkoff1@yandex.ru>
2022-11-15 14:41:37 +0300

    Доп задачи

--- script.js
+++ script.js
@@ -79,6 +79,51 @@ function longest(s1, s2) {
   let text = str.split('').sort().join('');
   return text
 }
+// Камень ножницы Бумага
+// Давайте играть! Вы должны вернуть, какой игрок выиграл! В случае ничьей возврат Draw!.
+
+// Примеры (Ввод1, Ввод2 --> Выход):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+const rps = (p1, p2) => {
+  if (p1 == 'scissors' && p2 == 'paper' || p1 == 'paper' && p2 == 'rock' || p1 == 'rock' && p2 == 'scissors'){
+    return "Player 1 won!";
+  }
+  if (p2 == 'scissors' && p1 == 'paper' || p2 == 'paper' && p1 == 'rock' || p2 == 'rock' && p1 == 'scissors'){
+    return "Player 2 won!";
+  }
+  if (p1 == 'scissors' && p2 == 'scissors' || p1 == 'paper' && p2 == 'paper' || p1 == 'rock' && p2 == 'rock'){
+    return "Draw"
+  }
+};
+// Сложение элементов массива с возведением в квадрат
+function squareSum(numbers){
+  let sum=0;
+  array = numbers.map((num) => num * num);
+  array.forEach(function(elem) {
+     sum+=elem;
+  });
+  return sum
+}
 
 
 
+// Добро пожаловать. В этой ката вас просят возвести в квадрат каждую цифру числа и соединить их.
+// Например, если мы пропустим через функцию 9119, получится 811181, потому что 9 2 равно 81, а 1 2 равно 1.
+// Примечание . Функция принимает целое число и возвращает целое число.
+
+function squareDigits(num){
+  num = String(num)
+  num = num.split('');
+  let text = '';
+  let answer = num.reduce((prew,item) => {
+    item = item * item;
+    return text = text+item;
+  },-1)
+  return +answer;
+}
+
+
+squareDigits(3212)
