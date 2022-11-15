edec372e389f1df25335de38f79a897a2fd3959b
Artem <a.bychkoff1@yandex.ru>
2022-11-15 12:46:45 +0300

    В порядке убывания

--- script.js
+++ script.js
@@ -13,38 +13,61 @@
 // "(( @"     =>  "))(("
 
 function duplicateEncode(word) {
-    word = word.toLowerCase();
-    
-    let counter = 0;
-    var array = word.split("");
-    let array2 = Array.from(array);
-    let uniqSet = Array.from(new Set(array));
-    for (let i = 0; i < uniqSet.length; i++) {
-      for (let j = 0; j < array.length; j++) {
-        if (uniqSet[i] === array[j]) {
-          counter++;
-        }
-      }
-      if (counter > 1) {
-        for (let j = 0; j < array.length; j++) {
-          if (uniqSet[i] === array[j]) {
-              array2[j] = ")";
-          }
-        }
-        counter = 0;
-      } else {
-        for (let j = 0; j < array.length; j++) {
-          if (uniqSet[i] === array[j]) {
-              array2[j] = "(";
-          }
-        }
-  
-        counter = 0;
+  word = word.toLowerCase();
+
+  let counter = 0;
+  var array = word.split("");
+  let array2 = Array.from(array);
+  let uniqSet = Array.from(new Set(array));
+  for (let i = 0; i < uniqSet.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (uniqSet[i] === array[j]) {
+        counter++;
       }
     }
-    text = array2.join("");
-    return text;
-  }
+    if (counter > 1) {
+      for (let j = 0; j < array.length; j++) {
+        if (uniqSet[i] === array[j]) {
+          array2[j] = ")";
+        }
+      }
+      counter = 0;
+    } else {
+      for (let j = 0; j < array.length; j++) {
+        if (uniqSet[i] === array[j]) {
+          array2[j] = "(";
+        }
+      }
 
-duplicateEncode("TTTTdT(TTTTwTTT");
-// ))))()())))()))
+      counter = 0;
+    }
+  }
+  text = array2.join("");
+  return text;
+}
+
+// В порядке убывания
+//Ваша задача состоит в том, чтобы создать функцию, которая может принимать любое неотрицательное целое число в качестве аргумента и возвращать его с цифрами в порядке убывания.
+//По сути, переставьте цифры, чтобы получить максимально возможное число.
+// Примеры:
+// Вход: 42145 Выход:54421
+
+// Вход: 145263 Выход:654321
+
+// Вход: 123456789 Выход:987654321
+
+function descendingOrder(n) {
+  const array = String(n).split("");
+  array.sort(function (a, b) {
+    return b - a;
+  });
+  n = +array.join("");
+  return n;
+}
+
+descendingOrder(123456789);
+
+
+
+
+
